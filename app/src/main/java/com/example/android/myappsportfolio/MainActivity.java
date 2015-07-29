29a1ebabf47ec89b6ec96e8f567d035d1b45cbb1
Jayashree Nagarajan <jayashree.nagarajan@gmail.com>
2015-07-29 14:46:15 -0700

    Made some corrections and modifications to the project after the code review from Udacity

--- app/src/main/java/com/example/android/myappsportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappsportfolio/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.android.myappsportfolio;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,37 +16,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    private Toast toast;
 
     public void DisplayMessage(View view)
     {
         Context context = getApplicationContext();
         Button button = (Button)view;
-        String msg = "This will launch my " + button.getText().toString() + " App ";
-        //CharSequence text = "Hello toast!";
-        int duration = 5;
+        //String msg = "This will launch my " + button.getText().toString() + " App ";
 
-        Toast toast = Toast.makeText(context, msg, duration);
+        String msg = getString(R.string.toast_msg) + " " + button.getText().toString();
+        if(toast != null)
+        {
+            toast.cancel();
+        }
+        toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
         toast.show();
     }
 }
